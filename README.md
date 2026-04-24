91a56070667b87ccfe0ece8ec136bc541ee4b041
Morion178 <musatmario00@gmail.com>
2026-04-24 16:17:58 +0300

    Working physical simulation, veryfing port comm

--- README.md
+++
@@ -1,1 +0,0 @@
-# Code the Future 2026
--- VirtualMoto/VirtualMoto/Work/slprj/sim/PRNDLController/PRNDLController_capi.h
+++
@@ -1,6 +0,0 @@
-#ifndef PRNDLController_capi_h_
-#define PRNDLController_capi_h_
-#include "PRNDLController.h"
-extern void PRNDLController_InitializeDataMapInfo ( ejdirckwac * const
-fdopgmryi2 , pxshinjyri * localDW , void * sysRanPtr , int contextTid ) ;
-#endif
