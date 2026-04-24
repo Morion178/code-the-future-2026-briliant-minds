b81c41fa6d696443329ba55f5bdaf0e2895542f4
Stefania1305 <alexandraroman.stefania2005@gmail.com>
2026-04-24 20:40:37 +0300

    versiune2test
    versiune2test

--- MotoTester.java
+++ MotoTester.java
@@ -9,39 +9,40 @@ public class MotoTester {
         InetAddress address = InetAddress.getByName("localhost");
         Random random = new Random();
 
-        System.out.println("Pornire Simulare Dinamică (Accelerare -> Croazieră -> Frânare)...");
+        System.out.println("Pornire Simulare (6 Parametri: RPM, Vit, Temp, CO2, Consum, Honk)...");
 
+        for (int i = 0; i < 1000; i++) {
+            int rpm, viteza, honk;
+            double l100;
 
-        for (int i = 0; i < 100; i++) {
-            int rpm, viteza;
-            double temp = 90.0 + random.nextDouble() * 5;
-            int emisii = 200 + random.nextInt(200);
-
-            if (i < 30) {
-                rpm = 1500 + (i * 200);
-                viteza = 10 + (i * 3);
-                emisii += 400;
-            } else if (i < 70) {
-                rpm = 3000 + random.nextInt(200);
-                viteza = 100 + random.nextInt(5);
-                emisii -= 100;
+            if (i % 60 < 30) {
+                // MOD ECO: RPM mic, Viteza mare
+                rpm = 2200 + random.nextInt(300);
+                viteza = 95 + random.nextInt(10);
+                l100 = 3.8 + random.nextDouble();
             } else {
-                rpm = 1000 + (100 - i) * 10;
-                viteza = Math.max(0, 110 - (i - 70) * 5);
+                // ACCELERARE
+                rpm = 5500 + random.nextInt(1000);
+                viteza = 40 + random.nextInt(30);
+                l100 = 9.5 + random.nextDouble();
             }
 
-            String testData = rpm + "," + viteza + "," + String.format("%.1f", temp) + "," + emisii;
-            byte[] buffer = testData.getBytes();
+            // Honk activ intermitent la fiecare 25 de pachete
+            honk = (i % 25 < 4) ? 1 : 0;
 
+            String data = rpm + "," + viteza + "," +
+                    String.format("%.1f", 92.0 + random.nextDouble()*3).replace(",", ".") + "," +
+                    (250 + random.nextInt(100)) + "," +
+                    String.format("%.2f", l100).replace(",", ".") + "," +
+                    honk;
+
+            byte[] buffer = data.getBytes();
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 5005);
             socket.send(packet);
 
-            System.out.println("Trimis: " + testData + (i >= 30 && i < 70 ? " [MOD ECO AȘTEPTAT]" : ""));
-
+            System.out.println("Trimis: " + data + (honk == 1 ? " [HONK!]" : ""));
             Thread.sleep(200);
         }
-
-        System.out.println("Simulare finalizată.");
         socket.close();
     }
 }
